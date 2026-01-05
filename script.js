33432a6897b25e774596509d13f1c9e2ea277d24
am-dev-log <am2028315@gmail.com>
2026-01-05 23:01:49 +0300

    Added new pages

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
- document.getElementById('button').onclick = function() {
+document.getElementById('button').onclick = function() {
     window.location.href = 'tools.html';
   };
+document.getElementById('button2').onclick = function() {
+	window.location.href = 'acc.html';
+};
