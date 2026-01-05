bb72fd03be7916ab84d4585f3bdda22c29963cfd
am-dev-log <am2028315@gmail.com>
2026-01-05 23:11:42 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.getElementById('button').onclick = function() {
     window.location.href = 'tools.html';
   };
-document.getElementById('button2').onclick = function() {
+document.getElementById('button1').onclick = function() {
 	window.location.href = 'acc.html';
 };
