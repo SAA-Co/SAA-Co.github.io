6bfc5622341667bb3afa0371fd068bf9982e9611
am-dev-log <am2028315@gmail.com>
2026-02-12 13:13:24 +0300

    Sign up page

--- History/script.js
+++ History/script.js
@@ -3,3 +3,8 @@ const hmbtn = document.getElementById("hmbtn");
 hmbtn.addEventListener('click' ,() => {
     window.location.href = "../index.html"
 });
+const loginBtns = document.querySelectorAll('#loginbtn');
+        if (loginBtns && loginBtns.length) {
+        const target = window.location.pathname.includes('/History/') ? '../sign-in.html' : 'sign-in.html';
+        loginBtns.forEach(btn => btn.addEventListener('click', () => { window.location.href = target; 
+        }))};
